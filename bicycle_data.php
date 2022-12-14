489b35fbcc207222bd28da7f108f05fec3c03be4
Encall <apichataimimpat@gmail.com>
2022-12-14 23:35:39 +0700

    Create bicycle_data.php

---
+++ bicycle_data.php
@@ -0,0 +1,20 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
+
+$conn = mysqli_connect('localhost','iot_admin','G@i!Km2j3CJ-B^8g','iot_database');
+
+$sql = "SELECT * FROM bicycle_data";
+$mysqli = mysqli_query($conn, $sql);
+$json_data = array();
+
+while($row = mysqli_fetch_assoc($mysqli))
+{
+    $json_data[] = $row;
+}
+
+echo json_encode(['bicycle_data'=>$json_data]);
+
+
+?>
